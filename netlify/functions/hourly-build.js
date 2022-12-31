89365b6db55d0b1c5ce44880df4e8cc5def6bc00
Sanya Singh <sanyasingh@Sanyas-MBP.lan>
2022-12-30 21:14:48 -0500

    Created cron job for hourly builds

---
+++ netlify/functions/hourly-build.js
@@ -0,0 +1,16 @@
+import { schedule } from "@netlify/functions"
+import fetch from "node-fetch";
+
+const BUILD_HOOK = process.env.BUILD_HOOK
+
+const handler = async function (event, context) {
+    console.log("Received event:", event)
+
+    return fetch(BUILD_HOOK, { method: 'POST' })
+        .then(() => ({
+            statusCode: 200,
+        }))
+        .catch(error => ({ statusCode: 422, body: String(error) }));
+};
+
+module.exports.handler = schedule("@hourly", handler);
